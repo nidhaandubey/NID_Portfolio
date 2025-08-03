f0b737bd51825f1dd0c187b47bb81dbf48a8e4df
nidhaandubey <nidhaandubey2@gmail.com>
2025-08-03 12:03:19 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import PortfolioPage from './PortfolioPage'
+import { Helmet } from "react-helmet";
 
+document.title = 'Nidhaan Dubey | Portfolio'
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <Helmet>
+        <title>Nidhaan Dubey | Portfolio</title>
+    </Helmet>,
   <React.StrictMode>
     <PortfolioPage />
   </React.StrictMode>,
